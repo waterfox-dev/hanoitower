2cf24ae1574e529872bc6327231aa98a591ad55a
Clément Baratin <bobbot.contact@gmail.com>
2021-11-09 16:22:44 +0100

    Reorganize element

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def initialisation(number_of_disk : int):
                 tower_dict[0][i].append('x')
     return tower_dict
 
-def last_elt(piquet : list, caractere = 'x'):
+def last_elt(piquet : list, caractere : str = 'x'):
     for i in range(1, len(piquet)):
         if piquet[-i] == caractere:
             return -i
     if piquet[0] == caractere : #On traite le premier élement du piquet
         return 0
+
+def first_disk(piquet : list, piquet_symbol : str = '-'):
+    for i in range(len(piquet)):
+        if piquet_symbol in piquet[i] :
+            return i
 
 def deplacer(disque : int, origine : int, destination : int):
     if origine not in [1,2,3] and destination not in [1,2,3]: #Vérificaiton de l'existence de la tour
@@ -23,24 +28,23 @@ def deplacer(disque : int, origine : int, destination : int):
     else :
         try :
             last_config                                    = list(dico_coups.keys())[-1]
-            disk                                           = dico_coups[last_config][origine][disque]
-            dico_coups[last_config][origine][disque]       = 'x'
+            disk                                           = first_disk(dico_coups[last_config][origine])            
             position                                       = last_elt(dico_coups[last_config][destination])
-            dico_coups[last_config][destination][position] = disk
+            dico_coups[last_config][destination][position] = dico_coups[last_config][origine][disk]
+            dico_coups[last_config][origine][disk]         = 'x'
             print(dico_coups)
         except IndexError :
             print(f"disque : {disque}\norigine : {origine}\ndestination : {destination}")
 
-def hanoi(n, a=1, b=2, c=3):
+def hanoi(n, i=1, j=2, k=3):
     if n==1:
-        print(f"Disque 1 de la tour {a} vers la tour {b}")
-        deplacer(disque = 0, origine = a-1, destination= b-1)
+        print(f"Disque 1 de la tour {i} vers la tour {j} ")
+        deplacer(0, i-1, j-1)
         return 
-    
-    hanoi(n-1, a, c, b)
-    print(f"Disque {n} de la tour {a} vers la tour {b} ")
-    deplacer(disque = n-1, origine = a-1, destination= b-1)
-    hanoi(n-1, c, b, a)
+    hanoi(n-1, i, k, j)
+    print(f"Disque {n} de la tour {i} vers la tour {j} ")
+    deplacer(-n, i-1, j-1)
+    hanoi(n-1, k, j, i)
 
 # Programme principal :
 if __name__ == "__main__":  
